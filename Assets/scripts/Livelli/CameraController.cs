0f90a3f2b47562c10360261e4240e3c23c085ffa
emarzano1 <ema.marzano1@stud.uniroma3.it>
2023-11-05 15:00:07 +0100

    .
    .

--- Assets/scripts/Livelli/CameraController.cs
+++ Assets/scripts/Livelli/CameraController.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControlloer : MonoBehaviour
+    public class CameraControlloer : MonoBehaviour
 {
     [SerializeField] private Transform Personaggio;
 
-   private void Update()
+    [SerializeField ]private bool seguiAsseX = false;
+
+    [SerializeField] private float maxY;
+    [SerializeField] private float minY;
+
+
+
+    private void Update()
     {
-        transform.position = new Vector3(Personaggio.position.x, Personaggio.position.y, transform.position.z);
+        float posX = seguiAsseX ? Personaggio.position.x : transform.position.x;
+        float posY;
+
+        if (seguiAsseX)
+        {
+            posY = transform.position.y;
+        }
+        else
+        {
+            posY = Mathf.Clamp(Personaggio.position.y, minY, maxY);
+        }
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
     }
 }
+
+
+
